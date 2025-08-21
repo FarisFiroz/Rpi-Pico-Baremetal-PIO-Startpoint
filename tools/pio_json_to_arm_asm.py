f278b282d4184a8eb669279776758cfb4724a8ac
FarisFiroz <77905112+FarisFiroz@users.noreply.github.com>
2025-08-20 21:29:23 -0400

    Add functionality to support multiple PIO programs in a single json

--- tools/pio_json_to_arm_asm.py
+++ tools/pio_json_to_arm_asm.py
@@ -5,16 +5,15 @@ path = Path("build_garbage/program1.json")
 with open("build_garbage/program1.json", "r") as f:
     data = json.load(f)
 
-instructions_list_dicts = data["programs"][0]["instructions"]
-
-final_output = ""
-for i, item in enumerate(instructions_list_dicts):
-    instruction_hex = next(iter(item.values()))
-    prefix = ".hword 0x"
-    if i > 0:
-        final_output += "\n"
-    final_output += (prefix + instruction_hex)
-
-print("program name: " + data["programs"][0]["name"])
-with open("build_garbage/" + data["programs"][0]["name"] + ".s", 'w') as f:
-    f.write(final_output)
+for program in data["programs"]:
+    final_output = ""
+    for i, item in enumerate(program["instructions"]):
+        instruction_hex = next(iter(item.values()))
+        prefix = ".hword 0x"
+        if i > 0:
+            final_output += "\n"
+        final_output += (prefix + instruction_hex)
+    
+    print("program name: " + program["name"])
+    with open("build_garbage/" + program["name"] + ".s", 'w') as f:
+        f.write(final_output)
